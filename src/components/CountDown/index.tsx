b5445f4d0b8235bd8d4ed25a602aa3bf5e2fb6bd
Lucas Pilar <lucas.npilar@hotmail.com>
2026-04-03 19:11:04 -0300

    feat(context): adiciona o context para carregar o state em toda aplicação

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,10 @@
+import { useTaskContent } from '../../contexts/TaskContext/useTaskContext';
 import styles from './Styles.module.css';
 
 export function CountDown() {
-  return <div className={styles.container}>00:00</div>;
+  const { state } = useTaskContent();
+
+  return (
+    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
+  );
 }
