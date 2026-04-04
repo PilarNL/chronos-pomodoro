4b3fb8bf70f1f2c0c70501d932ecdbb743d38dea
Lucas Pilar <lucas.npilar@hotmail.com>
2026-04-04 13:08:46 -0300

    refactor(mainform): uso de ref para captura de input

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,18 +2,27 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles/Index';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
 
 import styles from './styles.module.css';
 
 export function MainForm() {
+  const TaskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(TaskNameInput.current?.value);
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleCreateNewTask} className={styles.form}>
       <div className={styles.formRow}>
         <DefaultInput
           labelText='Tarefa'
           id='input'
           type='text'
-          placeholder='Digite sua tarefa'
+          placeholder='O que você vai fazer?'
+          ref={TaskNameInput}
         />
       </div>
       <div className={styles.formRow}>
